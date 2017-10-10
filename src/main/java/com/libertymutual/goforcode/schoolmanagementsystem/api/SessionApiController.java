0dc8bdac4fb52722d75c7936e0825e40c2a5483b
Roman Galanti <roman.galanti@libertymutual.com>
2017-10-10 15:40:56 -0700

    Updated security configuration.

--- src/main/java/com/libertymutual/goforcode/schoolmanagementsystem/api/SessionApiController.java
+++ src/main/java/com/libertymutual/goforcode/schoolmanagementsystem/api/SessionApiController.java
@@ -1,50 +1,84 @@
 package com.libertymutual.goforcode.schoolmanagementsystem.api;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.libertymutual.goforcode.schoolmanagementsystem.models.User;
-import com.libertymutual.goforcode.schoolmanagementsystem.repositories.UserRepository;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/api")
-@Api(description="This controller deals with loggin in, and session information.")
+@Api(description = "This controller deals with loggin in, and session information.")
 public class SessionApiController {
 
-	private UserRepository userRepo;
-	private PasswordEncoder encoder;
+	// private UserRepository userRepo;
+	// private PasswordEncoder encoder;
 
-	public SessionApiController(UserRepository userRepo, PasswordEncoder encoder) {
-		this.userRepo = userRepo;
-		this.encoder = encoder;
+	private UserDetailsService userDetails;
+	private AuthenticationManager authenticator;
+
+	public SessionApiController(UserDetailsService userDetails, AuthenticationManager authenticator) {
+		this.userDetails = userDetails;
+		this.authenticator = authenticator;
 	}
 
-	@ApiOperation(value = "Login as a user")
-	@PostMapping("login")
-	public User login(@RequestBody User user) {
-		String email = user.getEmail();
-		String password = user.getPassword();
-		User userInDatabase = new User();
-		if (userRepo.findByEmail(email) != null) {
-			userInDatabase = userRepo.findByEmail(email);
+	@ApiOperation(value = "Log a user in.")
+	@PutMapping("/login")
+	public Boolean login(@RequestBody Credentials credentials) {
+		System.out.println("login method ran");
 
-			String encryptedPassword = userInDatabase.getPassword();
-			// encryptedPassword = encoder
+		UserDetails details = userDetails.loadUserByUsername(credentials.getUsername());
 
-			if (encryptedPassword == password) {
-				// user is a match
-				return user;
-			}
+		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(details,
+				credentials.password, details.getAuthorities());
 
-			// userRepo.findByPassword(password).equals(encoder.encode(password));
+		authenticator.authenticate(token);
+		if (token.isAuthenticated()) {
+			SecurityContextHolder.getContext().setAuthentication(token);
 		}
-		return user;
+		;
+		return token.isAuthenticated();
+	}
+
+	@ApiOperation(value = "Logout a user.")
+	@DeleteMapping("/logout")
+	public Boolean logout(Authentication auth, HttpServletRequest request, HttpServletResponse response) {
+		new SecurityContextLogoutHandler().logout(request, response, auth);
+		return true;
+	}
+
+	static class Credentials {
+		private String username;
+		private String password;
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
 	}
 
 }
--- src/main/java/com/libertymutual/goforcode/schoolmanagementsystem/configuration/SecurityConfiguration.java
+++ src/main/java/com/libertymutual/goforcode/schoolmanagementsystem/configuration/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.libertymutual.goforcode.schoolmanagementsystem.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,25 +16,24 @@ import com.libertymutual.goforcode.schoolmanagementsystem.services.SchoolUserDet
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
 	private SchoolUserDetailsService schoolUserDetailsService;
 	
-	//Looks up users
 		public SecurityConfiguration(SchoolUserDetailsService schoolUserDetailsService) {
 			this.schoolUserDetailsService = schoolUserDetailsService;
 		
 		}
 		
-//		@Override
-//		protected void configure(HttpSecurity http) throws Exception {
-//			http
-//				//.csrf().disable()
-//				.authorizeRequests()
-//					.antMatchers("/", "/css/**", "/js/**", "/signup/**").permitAll()
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+				.csrf().disable()
+				.authorizeRequests()
+					.antMatchers("/**").permitAll()
 //					.antMatchers("/invoices/**").hasAnyRole("ADMIN", "ACCOUNTANT")
 //					.antMatchers("/billing-records/**").hasAnyRole("ADMIN", "CLERK")
 //					.antMatchers("/admin/**").hasRole("ADMIN") 	
-//					.anyRequest().authenticated() //any request that comes through security pipeline has to be authenticated
+					.anyRequest().authenticated(); //any request that comes through security pipeline has to be authenticated
 //				.and()	
 //				.formLogin();							
-//		}
+		}
 		
 	@Bean
 	public PasswordEncoder passwordEndoder() {
