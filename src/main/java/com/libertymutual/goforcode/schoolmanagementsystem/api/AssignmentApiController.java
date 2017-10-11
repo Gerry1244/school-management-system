a9d092265dc42c5b881311b18d3f8d66bbc288e1
Jon Podosek <jpodosek@gmail.com>
2017-10-11 12:34:23 -0700

    Added createAssignmentAndAssociateStudent method

--- src/main/java/com/libertymutual/goforcode/schoolmanagementsystem/api/AssignmentApiController.java
+++ src/main/java/com/libertymutual/goforcode/schoolmanagementsystem/api/AssignmentApiController.java
@@ -16,6 +16,8 @@ import com.libertymutual.goforcode.schoolmanagementsystem.models.Assignment;
 import com.libertymutual.goforcode.schoolmanagementsystem.models.Student;
 import com.libertymutual.goforcode.schoolmanagementsystem.models.Teacher;
 import com.libertymutual.goforcode.schoolmanagementsystem.repositories.AssignmentRepository;
+import com.libertymutual.goforcode.schoolmanagementsystem.repositories.StudentRepository;
+import com.libertymutual.goforcode.schoolmanagementsystem.repositories.TeacherRepository;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -26,9 +28,11 @@ import io.swagger.annotations.ApiOperation;
 public class AssignmentApiController {
 	
 	private AssignmentRepository assignmentRepo;
+	private StudentRepository studentRepo;
 	
-	public AssignmentApiController(AssignmentRepository assignmentRepo) {
+	public AssignmentApiController(AssignmentRepository assignmentRepo, StudentRepository studentRepo) {
 		this.assignmentRepo = assignmentRepo;
+		this.studentRepo = studentRepo;
 		assignmentRepo.save(new Assignment("Essay on some dumb topic"));
 	}
 	
@@ -41,8 +45,16 @@ public class AssignmentApiController {
 	@ApiOperation(value = "Create a new assignment, and associate it to all students under the teacher.")
 	@PostMapping("")
 	public Assignment create(@RequestBody Assignment assignment, @RequestBody Teacher teacher) {
-		
-		return assignmentRepo.save(assignment);
+		// find all students associated with teacher
+		List<Student> students;
+		try {
+			students = studentRepo.findByTeacher(teacher);
+			assignment.setStudents(students);
+			return assignmentRepo.save(assignment);
+		} catch (EmptyResultDataAccessException erdae) {
+			return null;
+		}
+
 	}
 	
 	@ApiOperation(value = "Delete an assignment.")
--- src/main/java/com/libertymutual/goforcode/schoolmanagementsystem/repositories/StudentRepository.java
+++ src/main/java/com/libertymutual/goforcode/schoolmanagementsystem/repositories/StudentRepository.java
@@ -1,11 +1,16 @@
 package com.libertymutual.goforcode.schoolmanagementsystem.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.libertymutual.goforcode.schoolmanagementsystem.models.Student;
+import com.libertymutual.goforcode.schoolmanagementsystem.models.Teacher;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+	
+	List<Student> findByTeacher(Teacher teacher);
 
 }
