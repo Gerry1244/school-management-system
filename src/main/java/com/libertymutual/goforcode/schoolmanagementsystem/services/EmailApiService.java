d6452c9b857e561951be3f0cc4a0251a9a45926a
Jon Podosek <jpodosek@gmail.com>
2017-10-25 09:49:01 -0700

    Updated email service method. It now works. Problem was the MailGun Api service only sends to authorized users with a max of 5 for the free service. So it's now hardcoded to send to schooltiy@gmail.com

--- src/main/java/com/libertymutual/goforcode/schoolmanagementsystem/services/EmailApiService.java
+++ src/main/java/com/libertymutual/goforcode/schoolmanagementsystem/services/EmailApiService.java
@@ -16,11 +16,10 @@ public class EmailApiService {
 
 	public JsonNode sendSimpleMessage(String email) throws UnirestException {
 		String html = "<h1>Welcome to School Management System!</h1>\r\n"
-		+ "A new account has been created for you to manage your school data via the School Management System.<br>\r\n"
-		+ "Please navigate to the School management system and login with this email as your username and your password.<br>\r\n"
-		+ "<b>Your password is: password</b><br>\r\n"
+		+ "A new account: " + email + "has been created.<br>\r\n" 
+		+ "<b>The account's password is defaulted to: password</b><br>\r\n"
+		+ "Please forward this email to " + email + " and advise them to change their password immediately. This can be found on the preference tab.<br>\r\n"
 		+ "Please navigate to Home > Preferences > Update Password to update your password immediately. <br>\r\n";
-
 
 		HttpResponse<JsonNode> request = Unirest
 				.post("https://api.mailgun.net/v3/" + "sandboxf39f405f504940fb9ce6cb2115b5d2e9.mailgun.org"
